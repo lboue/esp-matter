d804b23f042dd345315814c931645479eefbd06e
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-05-04 21:26:00 +0200

    Update app_main.cpp

--- examples/solar_power/main/app_main.cpp
+++ examples/solar_power/main/app_main.cpp
@@ -16,6 +16,7 @@
 #include <common_macros.h>
 #include <app_priv.h>
 #include <app_reset.h>
+#include <app/clusters/electrical-energy-measurement-server/electrical-energy-measurement-server.h>
 #include <static-supported-temperature-levels.h>
 #if CHIP_DEVICE_CONFIG_ENABLE_THREAD
 #include <platform/ESP32/OpenthreadLauncher.h>
@@ -23,7 +24,7 @@
 
 static const char *TAG = "app_main";
 static uint16_t solar_power_endpoint_id = 0;
-static uint16_t temp_ctrl_endpoint_id = 0;
+//static uint16_t temp_ctrl_endpoint_id = 0;
 
 using namespace esp_matter;
 using namespace esp_matter::attribute;
@@ -123,6 +124,14 @@ extern "C" void app_main()
     cluster_t *device_energy_management_cluster = cluster::get(endpoint, chip::app::Clusters::DeviceEnergyManagement::Id);
     cluster::device_energy_management::feature::power_adjustment::add(device_energy_management_cluster);
 
+    /* EEM 
+    electrical_energy_measurement::config_t econfig;
+    auto cumulative_energy_feature = electrical_energy_measurement::feature::cumulative_energy::get_id();
+    auto imported_energy_feature = electrical_energy_measurement::feature::imported_energy::get_id();
+    esp_matter::cluster_t *energy_cluster = electrical_energy_measurement::create(electrical_sensor_endpoint, &econfig, CLUSTER_FLAG_SERVER,
+        cumulative_energy_feature | imported_energy_feature);
+    */
+
     /*
     // "Temperature Measurement", "Refrigerator and Temperature Controlled Cabinet Mode Select" are optional cluster for temperature_controlled_cabinet device type so we are not adding them by default.
     temperature_controlled_cabinet::config_t temperature_controlled_cabinet_config;
@@ -164,6 +173,57 @@ extern "C" void app_main()
     /* Matter start */
     err = esp_matter::start(app_event_cb);
     ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to start Matter, err:%d", err));
+
+    // SetMeasurementAccuracy
+
+	auto mask = chip::BitMask<chip::app::Clusters::ElectricalEnergyMeasurement::Feature>(
+        chip::app::Clusters::ElectricalEnergyMeasurement::Feature::kCumulativeEnergy, 
+        chip::app::Clusters::ElectricalEnergyMeasurement::Feature::kImportedEnergy);
+	auto optionalmask = chip::BitMask<chip::app::Clusters::ElectricalEnergyMeasurement::OptionalAttributes>(
+        chip::app::Clusters::ElectricalEnergyMeasurement::OptionalAttributes::kOptionalAttributeCumulativeEnergyReset);
+	auto energyMeasurementAccess = new chip::app::Clusters::ElectricalEnergyMeasurement::ElectricalEnergyMeasurementAttrAccess(mask,optionalmask);
+
+	auto initerr = energyMeasurementAccess->Init();
+	if (chip::ChipError::IsSuccess(initerr) == false) {
+		ESP_LOGE(TAG, "energyMeasurementAccess->Init() ERR");
+	}
+	
+    int max_energy = 100;
+	chip::app::Clusters::ElectricalEnergyMeasurement::Structs::MeasurementAccuracyStruct::Type measurementAccuracy;
+	static chip::app::Clusters::ElectricalEnergyMeasurement::Structs::MeasurementAccuracyRangeStruct::Type sMeasurementAccuracyRange;
+
+	measurementAccuracy.measured = true;
+	measurementAccuracy.measurementType = chip::app::Clusters::detail::MeasurementTypeEnum::kElectricalEnergy;
+	measurementAccuracy.maxMeasuredValue = max_energy;
+	measurementAccuracy.minMeasuredValue = 0;
+
+	sMeasurementAccuracyRange.rangeMax = max_energy;
+	sMeasurementAccuracyRange.rangeMin = 0;
+	// sMeasurementAccuracyRange.percentMax.SetValue(energy_max_accuracy);
+	// sMeasurementAccuracyRange.percentMin.SetValue(energy_min_accuracy);
+	// sMeasurementAccuracyRange.percentTypical.SetValue(energy_typ_accuracy);
+	sMeasurementAccuracyRange.fixedMax.SetValue(187650443764698);
+	sMeasurementAccuracyRange.fixedMin.SetValue(187650443764696);
+	sMeasurementAccuracyRange.fixedTypical.SetValue(0);
+
+	measurementAccuracy.accuracyRanges = {sMeasurementAccuracyRange};
+
+    auto err_accu = chip::app::Clusters::ElectricalEnergyMeasurement::SetMeasurementAccuracy(solar_power_endpoint_id, measurementAccuracy);
+	if (chip::ChipError::IsSuccess(err_accu) == false) { 
+		//logger.E("SetMeasurementAccuracy ERR %u %u", err_accu.GetRange(), err_accu.GetValue()); // Change to log_e or your own logger
+        ESP_LOGI(TAG, "SetMeasurementAccuracy ERR");
+	}
+
+    int energy_value = 10000;
+    int endpoint_id = 1;
+    chip::app::Clusters::ElectricalEnergyMeasurement::Structs::EnergyMeasurementStruct::Type measurement;
+    measurement.startTimestamp.SetValue(1746386384-3600); // Change to your marked start EPOCH time of energy
+    measurement.endTimestamp.SetValue(1746386384); // Change to your own API to get EPOCH time (Or use systime APIs)
+    measurement.energy = energy_value;
+    
+    chip::DeviceLayer::StackLock lock;
+    auto success = chip::app::Clusters::ElectricalEnergyMeasurement::NotifyCumulativeEnergyMeasured(endpoint_id, {}, chip::Optional<chip::app::Clusters::ElectricalEnergyMeasurement::Structs::EnergyMeasurementStruct::Type> (measurement));
+    ESP_LOGI(TAG, "NotifyCumulativeEnergyMeasured() -> %u", success);
 
     //chip::app::Clusters::TemperatureControl::SetInstance(&sAppSupportedTemperatureLevelsDelegate);
 
