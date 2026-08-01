7b90ce5dadd3bc8702426ac264a12b453bef4aa1
Ludovic BOUÉ <938089+lboue@users.noreply.github.com>
2026-08-01 23:07:24 +0200

    example: add matter_project_config.h for Thermostat Controller identification

---
+++ examples/thermostat_controller/main/matter_project_config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Overrides the generic connectedhomeip defaults ("TEST_PRODUCT" / "TEST_VENDOR") with
+// names that identify this example in commissioners/apps (chip-tool, HomeKit-style
+// controllers, etc.).
+#define CHIP_DEVICE_CONFIG_DEVICE_PRODUCT_NAME "Thermostat Controller"
+#define CHIP_DEVICE_CONFIG_DEVICE_VENDOR_NAME "Espressif Systems"
